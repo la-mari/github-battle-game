798f350e27e5e76633296c21f5f54b75b5b0ba2b
la-mari <karima19@gmail.com>
2016-11-13 21:05:37 -0800

    react app setup and made first components

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+// var app = document.getElementById('app');
+// app.innerHTML = "Hello!"
+
+//new
+
+//var React = require('react');
+//var ReactDOM = require('react-dom');
+var USER_DATA = {
+  name: 'Mari Gonzalez',
+  username: 'la-mari',
+  image: 'https://avatars.githubusercontent.com/la-mari'
+}
+
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}}></img>
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <a href={'https://www.github.com/' +  this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function (){
+    return <div>{this.props.name}</div>
+  }
+});
+
+//avatar component will take care of encapsulating 
+//profile name component profile link and profile pic
+
+
+// var HelloWorld = React.createClass({
+// 	render: function () {
+// 		console.log(this.props)
+// 		return (
+// 			<div> Hello {this.props.name} </div>
+// 		)
+// 	}
+// });
+
+ReactDOM.render(
+  <HelloWorld name = "Mari"/>,
+  document.getElementById('app')
+);
+
+
+
+
+
+
+
+
+
